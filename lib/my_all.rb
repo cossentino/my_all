0eb582ea152ea0e3a13e30ec8755a8d6b4539521
Ian Cossentino <icossentino@gmail.com>
2020-11-10 15:07:22 -0500

    commit tree 042ccda3a73d8aa92e5c8f341fb8d3886b3302e5

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+    flag = true
+    collection.each do |e|
+        if yield(e)
+            next
+        else
+            flag = false
+            break
+        end
+    end
+    flag
 end
